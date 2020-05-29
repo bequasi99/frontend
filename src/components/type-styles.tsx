95d4d59b57e4355f3b272438700b2f1eb38c9615
adam wright <adam.wright90@yahoo.co.uk>
2020-05-29 12:15:06 +0100

    font size changes

--- src/components/type-styles.tsx
+++ src/components/type-styles.tsx
@@ -12,115 +12,60 @@ const TypeStyles = css`
   .small-font {
     line-height: 150%;
     letter-spacing: 0.02em;
-    ${fluidRange({
-      prop: "font-size",
-      fromSize: "12px",
-      toSize: "12px",
-    })};
-
-    @media ${props => props.theme.breakpoint.md} {
-      line-height: 150%;
-      letter-spacing: 0.02em;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "12px",
-        toSize: "12px",
-      })};
-    }
+    font-size: 12px;
 
     @media ${props => props.theme.breakpoint.lg} {
       line-height: 167%;
       letter-spacing: 0.005em;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "12px",
-        toSize: "15px",
-      })};
     }
 
     @media ${props => props.theme.breakpoint.xl} {
       line-height: 173%;
       letter-spacing: 0.01em;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "15px",
-        toSize: "15px",
-      })};
+      font-size: 15px;
     }
   }
 
   .medium-font {
     line-height: 130%;
     letter-spacing: 0.015em;
-    ${fluidRange({
-      prop: "font-size",
-      fromSize: "17px",
-      toSize: "16px",
-    })};
+    font-size: 17px;
 
     @media ${props => props.theme.breakpoint.md} {
       line-height: 130%;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "16px",
-        toSize: "19px",
-      })};
+      font-size: 16px;
     }
 
     @media ${props => props.theme.breakpoint.lg} {
       line-height: 128%;
       letter-spacing: 0.02em;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "19px",
-        toSize: "30px",
-      })};
+      font-size: 19px;
     }
 
     @media ${props => props.theme.breakpoint.xl} {
       line-height: 119%;
       letter-spacing: 0.015em;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "30px",
-        toSize: "30px",
-      })};
+      font-size: 30px;
     }
   }
 
   .large-font {
     line-height: 117%;
-    ${fluidRange({
-      prop: "font-size",
-      fromSize: "28px",
-      toSize: "28px",
-    })};
+    font-size: 28px;
 
     @media ${props => props.theme.breakpoint.md} {
       line-height: 110%;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "37px",
-        toSize: "49px",
-      })};
+      font-size: 37px;
     }
 
     @media ${props => props.theme.breakpoint.lg} {
       line-height: 110%;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "49px",
-        toSize: "72px",
-      })};
+      font-size: 49px;
     }
 
     @media ${props => props.theme.breakpoint.xl} {
       line-height: 107%;
-      ${fluidRange({
-        prop: "font-size",
-        fromSize: "72px",
-        toSize: "72px",
-      })};
+      font-size: 72px;
     }
   }
 `
