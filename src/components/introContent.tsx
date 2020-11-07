88becf16322028e7a8fce2a402f37c1d1c5fc6e0
bequasi99 <64752478+bequasi99@users.noreply.github.com>
2020-11-07 13:51:43 +0000

    Update introContent.tsx

--- src/components/introContent.tsx
+++ src/components/introContent.tsx
@@ -37,7 +37,7 @@ const IntroContent = () => {
     <div id="introContent">
       <div className="item">
         <p className="large-font">
-          be quasi uses design to to help people work more creatively and explore how people do their work.
+          be quasi uses design to help people work creatively and to explore how people do their work.
         </p>
       </div>
       <Scroll className="large-font">
