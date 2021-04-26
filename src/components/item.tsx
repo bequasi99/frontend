e87dacb964cf3414fb881139e8e5c8e8ffac73a4
Adam Green <adam.green@kcl.ac.uk>
2021-04-26 13:04:25 +0100

    fixed bug where button no longer shows on mobile

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -18,6 +18,9 @@ const Item = props => {
     >
       <h2 className="medium-font heading">{item.title.text}</h2>
       <p className="medium-font">{item.description.text}</p>
+      <div className="pdf-link-name-mobile">
+        <span className="small-font">{item.download.url.match(/\/([^\/]+)$/)[1]}</span>
+      </div>
       <div className="mobile-pdf">
         <div className="pdf-icon-wrapper">
           <img src={item.icon.url} className="pdf-icon" alt="pdf download" />
